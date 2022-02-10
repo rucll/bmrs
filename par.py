7a3cab8dafcb4b0c1d1cf2141dac5aa00365d960
v0lv0 <jindd8@gmail.com>
2022-02-09 23:01:57 -0500

    prase_str added

--- par.py
+++ par.py
@@ -10,10 +10,10 @@ def parse_file(file_name: str):
     return parse(s)
 
 
-def parse(s: str, wru='root') -> Node:
+def parse(s: str, wai='root') -> Node:
     if_index = s.find("if")
     # this_node = Node(value=None, name=wru + str(v))
-    this_node = Node(value=None, name=wru)
+    this_node = Node(value=None, name=wai)
 
     this_node.children = []
     then_index = -1
@@ -32,10 +32,10 @@ def parse(s: str, wru='root') -> Node:
                    after_if[:current_index].count('if')):
             current_index = after_if.find('then', current_index + 4)
         then_index = current_index + if_index + 3
-        this_node.children.append(parse(after_if[:current_index], wru='condition'))
+        this_node.children.append(parse(after_if[:current_index], wai='condition'))
     else:
         then_index = s.find("then")
-        this_node.children.append(parse(s[(if_index + 3):then_index], wru='condition'))
+        this_node.children.append(parse(s[(if_index + 3):then_index], wai='condition'))
 
     # find where then part ends
     then_start = then_index + 4
@@ -66,31 +66,35 @@ def parse(s: str, wru='root') -> Node:
     then_part = s[then_start:sep_index - 4]
     else_part = sbs
 
-    this_node.children.append(parse(then_part, wru='then'))
-    this_node.children.append(parse(else_part, wru='else'))
+    this_node.children.append(parse(then_part, wai='then'))
+    this_node.children.append(parse(else_part, wai='else'))
     return this_node
 
 
+# (full sentence, expression, current index)
+def parse_str(sentence: str, exp: str, i: int) -> bool:
+    return sentence[i + exp.count('S') - exp.count('P')] == ''.join(filter(str.islower, exp))
+
+
 # exp: "if if a(x) then TRUE else FALSE then if b(x) then c(x) else d(x) else c(P(x))"
-# string: "ababab"
-# ind: x val
-def evl_helper(r, s, x):
-    return None
-
-
-def evl(exp, s, x):
-    t = Tree(parse(s))
-    return None
+# Usage:
+# node.children[0~2] = condition node, then node, else node {node obj}
+# node.value = string of condition                          { P(x), S(x), }
+# node.name = type of node (Who Am I)                       { 'root', 'then', 'else', 'condition' }
 
 
 # root = parse("if if a(x) then true else false then if b(x) then c(x) else d(x) else c(P(x))")
-root = parse("if a(x) then if b(x) then g(S(x)) else d(x) else c(P(x))")
+# root = parse("if a(x) then if b(x) then g(S(x)) else d(x) else c(P(x))")
+#
+# tree = Tree(root=root)
+# print(tree)
+# print(root.children)
+# print(root.children[0].children)
+# then_cod = root.children[1].children
+# print(then_cod[0].value)
+# print(root.children[2].children)
 
-# vis(root)
-
-tree = Tree(root=root)
-print(tree)
-print(root.children)
-print(root.children[0].children)
-print(root.children[1].children)
-print(root.children[2].children)
+#    01234
+s = 'xfadf'
+exs = 'P(P(P(P(x)))))'
+print(parse_str(s, exs, 4))
