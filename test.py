ddbcf84f9b7b7afea18c96863b39af3516afb47e
Adam <adam.jardine@rutgers.edu>
2022-06-01 15:51:50 -0400

    added some testing code to test.py

--- test.py
+++ test.py
@@ -26,7 +26,7 @@
 from bmrs import bmrs
 
 x = 1
-sentence = "baaaa"
+sentence = "aabaaa"
 O = bmrs(sentence)
 
 exp = "IF IF e2(x) THEN False ELSE True THEN IF b(x) THEN b(P(P(x))) ELSE a(P(x)) ELSE b(P(x))"
@@ -35,4 +35,27 @@ O.add_to_dic("e1", exp )
 exp = "IF a(x) THEN False ELSE True"
 O.add_to_dic("e2", exp )
 
-print(O.evl("e1", x, sentence))
+
+# Testing from Adam
+
+# There are very often rules of the form a -> b / b _ (iterative), meaning that baaaa would be mapped to bbbbb
+# In such a case, we would want to define an output b using the following expression
+
+O.add_to_dic("b_o","IF b(x) THEN True ELSE b_o(p(x))")
+
+print(O.evl("b_o", 0, sentence))
+
+# Rule: b -> c / c X _ (X can be any string of segments)
+
+# follows_c = "IF c(p(x)) THEN True ELSE follows_c(p(x))"
+# a_o = "a(x)"
+# b_o = "if follows_c(x) THEN False ELSE b(x)"
+# c_o = "if c(x) THEN True ELSE IF b(x) THEN follows_c(x) ELSE False"
+
+# ldrule = bmrs("acaaba")
+# ldrule.add_to_dic("follows_c",follows_c)
+# ldrule.add_to_dic("a_o",a_o)
+# ldrule.add_to_dic("b_o",b_o)
+# ldrule.add_to_dic("c_o",c_o)
+
+# print(ldrule.evl("follows_c",0,"acaaba")) # < - This throws an "expression is not found" error
