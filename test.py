b47ec7cdd516c035e5714fcbfcba7ebe0d2fb2ec
Adam <adam.jardine@rutgers.edu>
2022-07-22 14:33:40 -0400

    testing

--- test.py
+++ test.py
@@ -9,9 +9,14 @@ from bmrs import bmrs
 # notes: 
 # bmrs stores dic
 # Expression 
-O = bmrs()
-O.add_to_dic("b_o","IF b(x) THEN True ELSE b_o(P(x))")
-
-      # 012345
-word = "aaaaaa"
-print(O.evl("b_o", 5, word))
+if __name__ == "__main__":
+    O = bmrs()
+    O.add_to_dic("a_o","IF b_o(x) THEN False ELSE a(x)")
+    O.add_to_dic("b_o","IF b(x) THEN True ELSE b_o(P(x))")
+    
+          # 012345
+    word = "aabaaa" # -> aabbbb
+    print(O.evl("b_o", 5, word))
+    print(O.evl("a_o", 5, word))
+    print(O.evl("b_o", 1, word)) # Should be false
+    print(O.evl("a_o", 1, word)) # Should be true
