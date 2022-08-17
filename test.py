3e5685ff14a0c1439b7d7a0936024311fd2d398c
Adam <adam.jardine@rutgers.edu>
2022-08-16 21:42:35 -0400

    added parsing bug example

--- test.py
+++ test.py
@@ -20,3 +20,9 @@ if __name__ == "__main__":
       print(O.evl("b_o", 1, word)) # Should be false
       print(O.evl("a_o", 1, word)) # Should be true
 
+
+      # *** Issues:
+      # The following hangs, because of the lowercase 'this'.
+      # We should have parse.py throw an error in this (and other) bad parse cases
+
+      O.add_to_dic("b_o","IF c(x) this FALSE ELSE IF b(x) THEN True ELSE b_i(P(x))")
