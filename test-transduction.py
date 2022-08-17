fb36b1beec5b88079d65da3ed9273caf53fcea25
Adam <adam.jardine@rutgers.edu>
2022-08-16 21:42:09 -0400

    added new_exp(), started transduce()

--- test-transduction.py
+++ test-transduction.py
@@ -21,3 +21,9 @@ if __name__ == "__main__":
       print(t.evl("b_o",1,word))
       print(t.evl("b_o",2,word))
       print(t.evl("b_o",3,word))
+
+
+      t.new_exp("a_o","IF b_o(x) THEN False ELSE a_i(x)")
+      t.new_exp("b_o","IF c_i(x) THEN FALSE ELSE IF b_i(x) THEN True ELSE b_i(P(x))")
+      t.new_exp("c_o","c_i(x)")
+      t.new_exp("a_i","c_i(x)")
--- transduction.py
+++ transduction.py
@@ -4,24 +4,52 @@ class Transduction(bmrs):
    """A class for defining the semantics of BMRS systems of equations as string to string transductions"""
 
    def __init__(self,sigma,gamma) -> True:
-       """Initialize an empty (describing the empty function) BMRS transduction.
-          sigma is input alphabet, gamma is output alphabet
-          s in sigma will be assigned symbol s_i
-          g in gamma will be assigned symbol g_o
-       """
-       bmrs.__init__(self)
+      """Initialize an empty (describing the empty function) BMRS transduction.
+         sigma is input alphabet, gamma is output alphabet
+         s in sigma will be assigned symbol s_i
+         g in gamma will be assigned symbol g_o
+      """
+
+      bmrs.__init__(self)
+      self.in_alph = sigma
+      self.out_alph = gamma
+
+      # Lists that keep track of which function names in dictionary correspond
+      # to input functions and which go to output functions
+      self.in_keys = []
+      self.out_keys = []
+
+      for s in sigma:
+         self.in_keys.append(s+"_i")
+         self.add_pair(s+"_i",s)
+
+      for g in gamma:
+         self.out_keys.append(g+"_o")
+         self.add_to_dic(g+"_o", 'False')
 
 
-       for s in sigma:
-           self.add_pair(s+"_i",s)
+   def transduce(self,s):
+      """
+      Transduce string s interpreting bmrs as an order-preserving transduction.
+      Essentially follows the rules in Bhaskar et al., 2021, which ultimately is derived
+      from those in Engelfriet and Hoogeboom 2001
+      """
+      indices = range(0,len(s)-1)
 
-       for g in gamma:
-           self.add_to_dic(g+"_o", 'False')
+      for i in indices:
+         pass #HERE
 
 
-    def transduce():
-        pass
+   def new_exp(self,f_name,f):
+      """
+      Add a new expression to the BMRS dictionary.
+      Note: This is just add_to_dic() with a failsafe so user doesn't overwrite the input functions
+      """
 
-    def new_exp():
-        # Note: this is just going to be add_to_dic() with a failsafe so you don't overwrite the input functions
-        pass
+      if f_name not in self.in_keys:
+         self.add_to_dic(f_name,f)
+      else:
+         raise LookupError("Cannot rewrite input alphabet functions")
+
+
+   # def __str__(self)
